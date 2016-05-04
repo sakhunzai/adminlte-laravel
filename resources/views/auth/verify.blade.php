708dd780eaebdcee879dd8a826c87814eb5141ec
shoukat <sakhunzai@gmail.com>
2016-05-04 12:09:09 +0500

    add missing partials, --all :(

---
+++ resources/views/auth/verify.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::layouts.auth')
+
+@section('htmlheader_title')
+   Verify your email 
+@endsection
+
+@section('content')
+<body class="hold-transition login-page">
+<div class="login-box" style="width:30%">
+
+    {{--@include('adminlte::partials.flash')--}}
+
+    You may <a href="{{ url('/login') }}" class="text-center">Login</a> into your account after verfication.
+
+</div><!-- /.login-box -->
+
+</body>
+
+@endsection
