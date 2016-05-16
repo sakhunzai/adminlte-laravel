b6aae987448dd92d0eb0bec2887c0323c82cd0c8
shoukat <sakhunzai@gmail.com>
2016-05-16 10:50:27 +0500

    fix img url

--- resources/views/layouts/partials/mainheader.blade.php
+++ resources/views/layouts/partials/mainheader.blade.php
@@ -34,7 +34,7 @@
                                     <a href="#">
                                         <div class="pull-left">
                                             <!-- User Image -->
-                                            <img src="{{ $header->profileImg }}" class="img-circle" alt="User Image"/>
+                                            <img src="{{ $header->profileImg }}" class="img-circle avatar" alt="User Image"/>
                                         </div>
                                         <!-- Message title and timestamp -->
                                         <h4>
